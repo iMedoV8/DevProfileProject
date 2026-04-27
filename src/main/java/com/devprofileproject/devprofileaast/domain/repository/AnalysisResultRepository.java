24cd037dde7e77f4349859c55edbcaadae24ea5d
Ahmed Mohamed <amhasan1407@gmail.com>
2026-04-27 22:40:59 +0300

    Fixed some errors

--- src/main/java/com/devprofileproject/devprofileaast/domain/repository/AnalysisResultRepository.java
+++ src/main/java/com/devprofileproject/devprofileaast/domain/repository/AnalysisResultRepository.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.devprofilebackend.devprofileback.domain.AnalysisResult;
+
 import com.devprofileproject.devprofileaast.domain.AnalysisResult;
 
 public interface AnalysisResultRepository extends JpaRepository<AnalysisResult, Long> {
